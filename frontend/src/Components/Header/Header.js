94b16c8dc7b72f403df44d917a3a3d1b703c4a27
sina-sh0734 <sinasheikhali.work@gmail.com>
2023-10-30 15:55:21 +0330

    Added typewriter-effect package and develop landing component

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Topbar from "../Topbar/Topbar";
+import Landing from "../Landing/Landing";
 
 import "./Header.css";
 
 export default function Header() {
   return (
     <header class="header">
-        <Topbar />
-        <Navbar />
+      <Topbar />
+      <Navbar />
+      <Landing />
     </header>
   );
 }
