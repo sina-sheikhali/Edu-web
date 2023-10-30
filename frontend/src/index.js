ddca9da65b8d32705d3e9091b9eb46ed6087ee07
sina-sh0734 <sinasheikhali.work@gmail.com>
2023-10-30 14:02:33 +0330

    Linking css pages and libraries

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 
-import { BrowserRouter } from "react-router-dom";
+import "./styles/reset.css";
+import "./styles/fonts.css";
+import "./styles/variables.css";
+import "./styles/helpers.css";
+import "./styles/defaults.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
--- frontend/src/pages/Index/Index.js
+++ frontend/src/pages/Index/Index.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Header from "../../Components/Header/Header";
 import "./Index.css";
 
 export default function Index() {
-  return <div>Index</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
