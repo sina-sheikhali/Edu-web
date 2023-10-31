09d526f0414fac7462c61475a1dfb1ad2f392859
sina-sh0734 <sinasheikhali.work@gmail.com>
2023-10-31 13:41:50 +0330

    Developed PopularCourses and PresellCourses components

--- frontend/src/pages/Index/Index.js
+++ frontend/src/pages/Index/Index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
 import "./Index.css";
+import LastCourses from "../../Components/LastCourses/LastCourses";
+import AboutUs from "../../Components/AboutUs/AboutUs";
+import PopularCourses from "../../Components/PopularCourses/PopularCourses";
+import PresellCourses from "../../Components/PresellCourses/PresellCourses";
+import LastArticles from "../../Components/LastArticle/LastArticles";
 
 export default function Index() {
   return (
     <div>
       <Header />
+      <LastCourses />
+      <AboutUs />
+      <PopularCourses />
+      <PresellCourses />
+      <LastArticles />
     </div>
   );
 }
